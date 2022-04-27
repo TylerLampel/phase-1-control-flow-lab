baeccf1eb92db52ec43961a73c9d400ae1546f98
Tyler Lampel <lampel13@gmail.com>
2022-04-26 21:56:21 -0400

    commit tree aa77ba4ad68937e2c0720c3aff40d2c0ed42c82d

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let greeting
+  if (number < 400) {
+    greeting = 'This one is on me!';
+  } else if (number <= 2000) {
+    greeting = 'That will be twenty bucks.';
+  } else if (number <= 2500)  {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    greeting = 'No can do.';
+  }
+  return greeting
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityMessage= city=== 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMessage
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch(tip) {
+    case 'generous':
+      charm="Thank you so much.";
+      break;
+    case 'not as generous':
+      charm= "Thank you.";
+      break;
+    default:
+      return 'Bye.'
+      break;
+  }
+  return charm
 }
